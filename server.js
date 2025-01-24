870dd913997f11688ca71b96f81143397067431a
dimaszisby <dimaszisby@gmail.com>
2025-01-24 14:21:52 +0700

    update: [server] middleware security imports and clean-ups

--- server.js
+++ server.js
@@ -1,36 +1,73 @@
+// server.js
+
+// Internals
 const express = require("express");
 const sequelize = require("./config/db");
 const cors = require("cors");
 const dotenv = require("dotenv");
+
+// Security Middlewares
+const helmet = require("helmet");
+const xss = require("xss-clean"); // Additional security
+const hpp = require("hpp"); // Prevent HTTP parameter pollution
+const { globalRateLimiter } = require("./middleware/rate-limiter");
+
+// Routes
 const authRoutes = require("./routes/auth-routes");
 const metricRoutes = require("./routes/metric-routes");
 const metricCategoryRoutes = require("./routes/metric-category-routes");
 const metricSettingsRoutes = require("./routes/metric-settings-routes");
 const metricLogRoutes = require("./routes/metric-log-routes");
 
-// Load environment variables
+// other setups
+const errorHandler = require("./middleware/error-handler");
+
+/**
+ * * App Entry
+ * Execution Flow:
+ *  1. Load Env
+ *  2. Initializes Middlewares
+ *  3. Initializes Routes
+ *  4. Initialize Global Error handler
+ *  5. Start the server based on config/prompt
+ */
+
+// * Environment Variables
 dotenv.config();
 const app = express();
 
-// Middleware
-app.use(cors());
+// * Middlewares
 app.use(express.json());
 
-// Routes
+// Data Sanitization against XSS
+// TODO: Extended implementations
+app.use(xss());
+
+// Prevent HTTP Parameter Pollution
+// TODO: Extended implementations
+app.use(hpp());
+
+// TODO: Extended implementations
+app.use(cors());
+
+// Global Rate Limiter
+app.use(globalRateLimiter);
+
+// * Routes
+// Main Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/metrics", metricRoutes);
 app.use("/api/v1/categories", metricCategoryRoutes);
 
-// Nested Routes for Metric Settings and Logs
+// Nested Routes
 app.use("/api/v1/metrics", metricSettingsRoutes);
 app.use("/api/v1/metrics", metricLogRoutes);
 
-// Global Error Handler
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: "Something broke!", error: err.message });
-});
+// * Global Error Handler
+// Should be initialized after all midlewares and routes
+app.use(errorHandler);
 
+// * Server Mode Init
 // Only authenticate and start server if not in test environment
 if (process.env.NODE_ENV !== "test") {
   sequelize
