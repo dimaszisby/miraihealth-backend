e8201c19606e529a95130cb61b9000c34b2c38fa
dimaszisby <dimaszisby@gmail.com>
2025-02-12 15:09:02 +0700

    update: [config] config -> cleanups

--- config/config.js
+++ config/config.js
@@ -2,39 +2,40 @@
 module.exports = {
   development: {
     url: process.env.DEVELOPMENT_DATABASE_URL,
-    database: 'miraihealth',
-    host: process.env.DB_HOST || '127.0.0.1',
+    database: "miraihealth",
+    host: process.env.DB_HOST || "127.0.0.1",
     port: process.env.DB_PORT || 5432,
-    dialect: 'postgres',
+    dialect: "postgres",
     logging: console.log, // Enable detailed logging
     dialectOptions: {
       // Ensure search_path includes 'public'
-      options: '-c search_path=public'
+      options: "-c search_path=public",
+      ssl: false,
     },
     redis: {
-      host: process.env.REDIS_HOST || '127.0.0.1',
+      host: process.env.REDIS_HOST || "127.0.0.1",
       port: process.env.REDIS_PORT || 6379,
       password: process.env.REDIS_PASSWORD || undefined,
     },
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
-    database: 'miraihealth_test',
-    host: process.env.DB_HOST || '127.0.0.1',
+    database: "miraihealth_test",
+    host: process.env.DB_HOST || "127.0.0.1",
     port: process.env.DB_PORT || 5432,
-    dialect: 'postgres',
+    dialect: "postgres",
     logging: false, // Optional: Disable logging for cleaner test output
     redis: {
-      host: process.env.REDIS_HOST || '127.0.0.1',
+      host: process.env.REDIS_HOST || "127.0.0.1",
       port: process.env.REDIS_PORT || 6379,
       password: process.env.REDIS_PASSWORD || undefined,
     },
   },
   production: {
-    url: process.env.DATABASE_URL,
-    dialect: 'postgres',
+    url: process.env.PRODUCTION_DATABASE_URL,
+    dialect: "postgres",
     redis: {
-      host: process.env.REDIS_HOST || '127.0.0.1',
+      host: process.env.REDIS_HOST || "127.0.0.1",
       port: process.env.REDIS_PORT || 6379,
       password: process.env.REDIS_PASSWORD || undefined,
     },
