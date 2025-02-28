8dac25bb027a2caf052f386e6fe44673c2dc1252
dimaszisby <dimaszisby@gmail.com>
2025-02-28 15:48:28 +0700

    update: [db-validator] add validator for metric-category

--- src/utils/db-validators.ts
+++ src/utils/db-validators.ts
@@ -3,7 +3,7 @@
 import db from "../models/index.js";
 import AppError from "../utils/AppError.js";
 
-const { Metric } = db;
+const { Metric, MetricCategory } = db;
 
 /**
  * * Utility function to validate if a metric exists
@@ -49,4 +49,29 @@ export const validateMetricAccess = async (
     throw new AppError("Unauthorized access to metric stats", 403);
   }
   return metric;
+};
+
+/**
+ * * Utility function to validate if a metric category exists and owned by the requesting user
+ * @param userId - The ID of the user who owns the metric
+ * @param categoryId - The ID of the metric category
+ * @throws {AppError} If metric does not exist
+ */
+export const validateMetricCategoryAccess = async (
+  userId: string,
+  categoryId: string
+) => {
+  // First, fetch the metric regardless of the userId.
+  const metricCategory = await MetricCategory.findOne({
+    where: { id: categoryId },
+    attributes: ["id", "userId"],
+  });
+  if (!metricCategory) {
+    throw new AppError("Metric Category not found", 404);
+  }
+  // Then, if the metric is does not belong to the user, throw unauthorized.
+  if (metricCategory.userId !== userId) {
+    throw new AppError("Unauthorized access to metric category", 403);
+  }
+  return metricCategory;
 };
