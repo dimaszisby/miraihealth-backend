fa41453bc84a3eadb8f3c349ad2747139904de85
dimaszisby <dimaszisby@gmail.com>
2025-03-26 17:47:57 +0700

    add: [constants] zod-messages -> add zodd messages for zod schema

---
+++ src/constants/zod-messages.ts
@@ -0,0 +1,9 @@
+export const ZodMessages = {
+  user: {
+    usernameMin: "Username must be at least 3 characters",
+    emailInvalid: "Invalid email address",
+    passwordMin: "Password must be at least 6 characters",
+    passwordConfirmMin: "Password confirmation must be at least 6 characters",
+    passwordMismatch: "Passwords do not match",
+  },
+};
