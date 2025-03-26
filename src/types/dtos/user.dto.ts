03482809b46c2ce3ea9206c3033527e157bd39f0
dimaszisby <dimaszisby@gmail.com>
2025-03-26 17:50:53 +0700

    update: [types - dto] updating UserResponseDTO

--- src/types/dtos/user.dto.ts
+++ src/types/dtos/user.dto.ts
@@ -1,9 +1,17 @@
 // src/types/user.dto.ts
 
-export interface UserDTO {
+/**
+ * * Data Transfer Objects (DTO)
+ * For incoming/outgoing API contract.
+ */
+
+// Response when returning user (e.g., on login, profile)
+export interface UserResponseDTO {
   id: string;
   username: string;
   email: string;
-  isPublicProfile: boolean;
   role: "user" | "admin";
+  isPublicProfile: boolean;
+  createdAt: string;
+  updatedAt: string;
 }
