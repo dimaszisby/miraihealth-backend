6f6d1d569a5e9748ef9fef97c2263da17298a142
dimaszisby <dimaszisby@gmail.com>
2025-04-04 20:18:45 +0700

    add: [utils - mapper] adding mapper for metric-category

---
+++ src/utils/mappers/metric-category.mapper.ts
@@ -0,0 +1,34 @@
+// src/utils/mappers/metricCategory.mapper.ts
+
+import { MetricCategory } from "@/models/metric-category.model";
+import { MetricCategoryDomain } from "@/types/domain/metric-category.domain";
+import { MetricCategoryResponseDTO } from "@/types/dtos/metric-category.dto";
+
+/**
+ * * Mapper: Sequelize → Domain
+ */
+export const toDomainMetricCategory = (
+  metricCategory: MetricCategory
+): MetricCategoryDomain => ({
+  id: metricCategory.id,
+  name: metricCategory.name,
+  color: metricCategory.color,
+  icon: metricCategory.icon,
+  createdAt: metricCategory.createdAt!,
+  updatedAt: metricCategory.updatedAt!,
+  deletedAt: metricCategory.deletedAt ?? null,
+});
+
+/**
+ * * Mapper: Domain → DTO (for responses)
+ */
+export const toMetricCategoryResponseDTO = (
+  metricCategory: MetricCategoryDomain
+): MetricCategoryResponseDTO => ({
+  id: metricCategory.id,
+  name: metricCategory.name,
+  color: metricCategory.color,
+  icon: metricCategory.icon,
+  createdAt: metricCategory.createdAt.toISOString(),
+  updatedAt: metricCategory.updatedAt.toISOString(),
+});
