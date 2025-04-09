f50ec2883082b38dbd969d3dc0276f3cb4885cae
dimaszisby <dimaszisby@gmail.com>
2025-04-09 17:29:01 +0700

    add: [utils] add generic mapper for extended entity

---
+++ src/utils/mappers/generic.mapper.ts
@@ -0,0 +1,9 @@
+// src/utils/mappers/generic.mapper.ts
+
+export const extendDomain = <T extends object, U extends object>(
+  domain: T,
+  extras: Partial<U>
+): T & Partial<U> => ({
+  ...domain,
+  ...extras,
+});
