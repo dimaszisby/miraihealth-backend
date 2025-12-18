0170799f8e443e6f6549398a6051d6714622d10e
dimaszisby <dimaszisby@gmail.com>
2025-12-18 20:27:11 +0700

    update: [shared - middleware] overhauling validations  handling for zod schemas

--- src/shared/middleware/validated.ts
+++ src/shared/middleware/validated.ts
@@ -3,5 +3,22 @@ import type { z } from "zod";
 
 export const pickValidated =
   <S extends z.ZodTypeAny>(schema: S) =>
-  (req: Request) =>
-    req.validated as z.infer<S>;
+  (req: Request) => {
+    if (req.validated) {
+      return req.validated as z.infer<S>;
+    }
+
+    const parsed = schema.safeParse({
+      params: req.params,
+      query: req.query,
+      body: req.body,
+    });
+
+    if (!parsed.success) {
+      throw parsed.error;
+    }
+
+    const result = parsed.data as z.infer<S>;
+    req.validated = result;
+    return result;
+  };
